5ff879dd0f11f4e0f89fd8e2c27145d291a78b81
Davide Ciacco <davide.ciacco@crehub.it>
2018-02-26 11:12:54 +0100

    correct footer

--- wp-content/themes/prana/footer.php
+++ wp-content/themes/prana/footer.php
@@ -1,10 +1,10 @@
-  <!-- <footer id="footer" class="site-footer" role="contentinfo">
+  <footer id="footer" class="site-footer" role="contentinfo">
     <div class="container_16">
 
   © Copyright 2016 - <a href="http://em.qbdp.me/devlog/">CiaccoDavide's Dev(B)Log</a>
   <a href="http://designorbital.com/prana/" title="Prana Theme"> ⋅ Prana Theme</a> ⋅ Powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a> ⋅ Modded by <a href="http://ciaccodavi.de/" title="CiaccoDavide">Ciacco Davide</a>
 </div>
-  </footer> -->
+  </footer>
 
 </div> <!-- end .wrapper -->
 
